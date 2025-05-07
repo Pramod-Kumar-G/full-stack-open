fe1b8c01ba6177cd527280e195d09dc44642a40f
Pramod Kumar <pramodg2002@gmail.com>
2025-05-07 16:33:53 +0530

    implement solution for exercise 3.2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -28,6 +28,11 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const currentDateTime = new Date();
+  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${currentDateTime}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
