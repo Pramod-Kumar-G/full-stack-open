d3fe84378344c965454f9127acfa6c4bf40bb865
Pramod Kumar <pramodg2002@gmail.com>
2025-05-10 21:26:11 +0530

    implement solution for exercise 3.16

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -57,15 +57,12 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(person => {
       response.status(204).end()
     })
-    .catch(error => {
-      console.log(error.message)
-      response.status(500).end()
-    })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -89,6 +86,17 @@ app.post('/api/persons', (request, response) => {
       response.json(savedPerson)
     })
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" })
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
