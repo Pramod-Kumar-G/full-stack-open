49d2807b6eeaa8c6dc78ace131ed80555102f98b
Pramod Kumar <pramodg2002@gmail.com>
2025-05-10 21:42:53 +0530

    implement solution for exercise 3.18

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -13,29 +13,6 @@ morgan.token('body', function (req, res) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-  {
-    "id": "1",
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": "2",
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": "3",
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": "4",
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
-
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons)
@@ -43,18 +20,22 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-  const currentDateTime = new Date();
-  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${currentDateTime}</p>`)
+  Person.find({}).then(persons => {
+    const currentDateTime = new Date();
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${currentDateTime}</p>`)
+  });
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(p => p.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        response.status(404).end()
+      } else {
+        response.json(person)
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
