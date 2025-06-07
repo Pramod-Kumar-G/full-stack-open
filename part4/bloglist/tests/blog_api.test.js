ee6f4fd5eba341fec2e011374547d30638ea973c
Pramod Kumar <pramodg2002@gmail.com>
2025-06-07 11:15:32 +0530

    implement solution for exercise 4.9

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -31,23 +31,29 @@ beforeEach(async () => {
   await blogObject.save()
 })
 
-test.only('blogs are returned as json', async () => {
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
 
-test.only('all blogs are returned', async () => {
+test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
   assert.strictEqual(response.body.length, 2)
 })
 
-test.only('a specific blog is within the returned blogs', async () => {
+test('a specific blog is within the returned blogs', async () => {
   const response = await api.get('/api/blogs')
 
   const titles = response.body.map(e => e.title)
   assert.strictEqual(titles.includes('React patterns'), true)
+})
+
+test.only('unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert(response.body[0].id)
 })
 
 after(async () => {
