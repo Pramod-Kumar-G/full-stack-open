061c252ccf6a4a5858a8a63df5ee948adeaf3cd5
Pramod Kumar <pramodg2002@gmail.com>
2025-07-25 23:07:21 +0530

    implement solution for exercise 5.17

---
+++ part5/bloglist-e2e/tests/bloglist.spec.js
@@ -0,0 +1,13 @@
+import { test, describe, expect, beforeEach } from "@playwright/test"
+
+describe('Blog App', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByText('Log in to application')).toBeVisible()
+    await expect(page.getByText('username')).toBeVisible()
+    await expect(page.getByText('password')).toBeVisible()
+  })
+})
