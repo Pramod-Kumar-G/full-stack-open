306ed2d1acd989b40dc3febe8f620e9ead79286b
Pramod Kumar <pramodg2002@gmail.com>
2025-08-08 19:13:10 +0530

    implement solution for exercise 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,11 @@ import { useSelector, useDispatch } from "react-redux";
 import { voteFor } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector(({ anecdotes, filter }) => {
+    return anecdotes.filter((a) =>
+      a.content.toLowerCase().includes(filter.toLowerCase()),
+    );
+  });
   const dispatch = useDispatch();
 
   const vote = (id) => {
