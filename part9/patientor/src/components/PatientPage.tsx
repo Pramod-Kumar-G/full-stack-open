546e4d179432a13ff4b8df470e1aa6cad5d2eba0
Pramod Kumar <pramodg2002@gmail.com>
2025-10-23 18:24:32 +0530

    implement solution for exercise 9.22

--- part9/patientor/src/components/PatientPage.tsx
+++ part9/patientor/src/components/PatientPage.tsx
@@ -1,25 +1,27 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import patientService from "../../services/patients";
-import { Patient } from "../../types";
+import patientService from "../services/patients";
+import { Patient } from "../types";
+import FemaleIcon from "@mui/icons-material/Female";
+import MaleIcon from "@mui/icons-material/Male";
 
 const PatientPage = () => {
   const [patient, setPatient] = useState<Patient>();
-  const { patientId } = useParams();
-  if (!patientId) {
+  const { id } = useParams();
+  if (!id) {
     return <div>No Patient Found</div>;
   }
   useEffect(() => {
-    patientService.getById(patientId).then((patients) => {
-      console.log(patients);
-      const patient = patients.find((p) => p.id === patientId);
+    patientService.getById(id).then((patient) => {
       setPatient(patient);
     });
   }, []);
-  console.log(patient);
   return (
     <div>
-      <h2>{patient?.name}</h2>
+      <h2>
+        {patient?.name}{" "}
+        {patient?.gender === "female" ? <FemaleIcon /> : <MaleIcon />}
+      </h2>
       <div>ssn: {patient?.ssn}</div>
       <div>occupation: {patient?.occupation}</div>
     </div>
